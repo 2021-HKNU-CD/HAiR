08842963c9a5498a0a6f93b0d76e484b1aa61e14
jinwoo1225 <jinwoo17962@gmail.com>
2021-05-13 13:06:51 +0900

    MaskOrientGenerator완성

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import numpy as np
-
-from faceSegmentation import FaceSegmentation, img_to_ndarray
-from calOrient import Orient, save_to_img
+from src.components.MaskOrientGenerator.faceSegmentation import FaceSegmentation, img_to_ndarray
+from src.components.MaskOrientGenerator.calOrient import Orient, save_to_img
 
 FS = FaceSegmentation('checkpoints/model.pt')
 mask = FS.image_to_mask(img_to_ndarray('99999.jpg'), 256, 512)
