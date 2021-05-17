71261c5fb30346bd00f99c2ad51726f02b67c64d
Hong Jinwoo <jinwoo17962@gmail.com>
2021-05-17 17:15:09 +0900

    Revert "Face features"

--- BBtest.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-from src.components.BoundingBox.BoundingBox import BoundingBox
-
-BB = BoundingBox(original_image=cv2.imread('data/test/iu.jpg'),
-                 facefeat_model_path='models/checkpoints/shape_predictor_68_face_landmarks.dat')
-
-BB.get_bounding_box()
--- README.md
+++ README.md
@@ -7,8 +7,3 @@
 ### 개발중...
 
 ### Documentation(https://www.notion.so/Documents-bbcec7a5a93b468c9adc3342e60209e1)
-
-# Used Repository
-https://github.com/raviranjan0309/Detect-Facial-Features
-
-https://github.com/kampta/face-seg
