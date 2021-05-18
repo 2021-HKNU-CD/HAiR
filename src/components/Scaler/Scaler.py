fca5d589803f0d203749683edd7c928ddb486d7e
Hong Jinwoo <jinwoo17962@gmail.com>
2021-05-19 00:57:52 +0900

    Revert "implement : Scaler.py"

--- src/components/Scaler/Scaler.py
+++ src/components/Scaler/Scaler.py
@@ -1,5 +1,4 @@
 import numpy as np
-import cv2
 
 class Scaler:
     def __init__(self, aligned_patch: np.ndarray, target_size: int = 512):
@@ -8,10 +7,7 @@ class Scaler:
         '''
         self.target_size = target_size
         self.aligned_patch = aligned_patch
-        if aligned_patch.shape[0] != aligned_patch.shape[1]:
-            raise Exception('aligned_patch의 가로 세로 크기가 동일하지 않습니다.')
-
-        self.origin_size = aligned_patch.shape[0]
+        self.origin_size = None
         pass
 
     def scale_forward(self) -> np.ndarray:
@@ -19,16 +15,11 @@ class Scaler:
         target_size(512)로 이미지 크기 변환한 후 반환
         return : target_size x target_size 크기의 이미지
         '''
-        return cv2.resize(self.aligned_patch,
-                          dsize=(self.target_size, self.target_size),
-                          interpolation=cv2.INTER_LINEAR)
+        pass
 
-    def scale_backward(self, style_changed_512) -> np.ndarray:
+    def scale_backward(self) -> np.ndarray:
         '''
         원래 크기로 변환후 반환
-        param style_changed_512 : MichiGAN을 통과해서 나온 512 x 512 이미지
         return : aligned_patch와 크기가 동일한 이미지
         '''
-        return cv2.resize(style_changed_512,
-                          dsize=(self.origin_size, self.origin_size),
-                          interpolation=cv2.INTER_LINEAR)
+        pass
