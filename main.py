0c614d6f5ca9b498a293314c5b436d2823d47821
Hong Jinwoo <jinwoo17962@gmail.com>
2021-05-18 23:10:41 +0900

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from src.components.MaskOrientGenerator.faceSegmentation import FaceSegmentation, img_to_ndarray
-from src.components.MaskOrientGenerator.calOrient import Orient, save_to_img
-
-FS = FaceSegmentation('checkpoints/model.pt')
-mask = FS.image_to_mask(img_to_ndarray('data/test/99999.jpg'), 256, 512)
-orient = Orient()
-orient_dense = orient.makeOrient(img_to_ndarray('data/test/99999.jpg'), mask)
-save_to_img(orient_dense, "99999")
