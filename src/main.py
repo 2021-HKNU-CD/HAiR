04807d374092001504d836164ab947db546e84cd
Hong Jinwoo <jinwoo17962@gmail.com>
2021-06-01 10:53:17 +0900

    fixed : typo

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from src.transformers.AppearanceTransformer import AppearanceTransformer 
 from src.transformers.ShapeStructureTransformer import ShapeStructureTransformer
 
-transformers = {'appreance' : AppearanceTransformer(), 'shape' : ShapeStructureTransformer()}
+transformers = {
+    'appearance': AppearanceTransformer(),
+    'shape': ShapeStructureTransformer()
+}
 mode = 'appearance'
 
 CameraInput = None
